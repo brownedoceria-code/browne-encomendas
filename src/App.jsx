998c136217d5731d96d53ed9198e635ce0780fa6
brownedoceria-code <browne.doceria@gmail.com>
2026-06-26 00:08:39 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect, useCallback, useRef } from "react";
+import { useState, useMemo, useEffect, useCallbck, useRef } from "react";
 
 // ─── Supabase ─────────────────────────────────────────────────────────────────
 const SUPABASE_URL = "https://ycyqdcpamifghuludkjr.supabase.co";
